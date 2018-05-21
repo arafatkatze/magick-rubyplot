8e577f7ee569bf22863b18c7cda75aab50e33c32
Arafat <arafat@iitkgp.ac.in>
2018-05-21 18:14:37 +0530

    Adding Gtk basic example. This example will be developed later to a more complete module for GUI Widget.

---
+++ lib/rubyplot/gtk/gtk_README.md
@@ -0,0 +1,1 @@
+set_window_position Gtk::Window::Position::CENTER
---
+++ lib/rubyplot/gtk/show_image.rb
@@ -0,0 +1,54 @@
+require 'RMagick'
+require 'gtk3'
+
+class RubyApp < Gtk::Window
+  def initialize
+    super
+
+    init_ui
+  end
+
+  def init_ui
+    override_background_color :normal, Gdk::RGBA.new(0.2, 0.2, 0.2, 1)
+    begin
+      bardejov = GdkPixbuf::Pixbuf.new file: 'log.png'
+      sfsdf = GdkPixbuf::Pixbuf.new file: 'logo.png'
+    rescue IOError => e
+      puts e
+      puts 'cannot load images'
+      exit
+    end
+
+    image1 = Gtk::Image.new pixbuf: bardejov
+    fixed = Gtk::Fixed.new
+    add fixed
+    fixed.put image1, 20, 20
+
+    button = Gtk::Button.new label: 'Quit'
+    button.set_size_request 80, 35
+    button.signal_connect 'clicked' do
+      puts 'hello world'
+
+      img = Magick::Image.read('log.png').first
+      img.resize_to_fit!(400, 400)
+      img.write('log.png')
+
+      image1 = Gtk::Image.new pixbuf: sfsdf
+      fixed.put image1, 30, 30
+    end
+
+    fixed.put button, 50, 50
+
+    set_title 'Quit button'
+    signal_connect 'destroy' do
+      Gtk.main_quit
+    end
+
+    set_default_size 300, 200
+    set_window_position(:center)
+    show_all
+  end
+end
+
+window = RubyApp.new
+Gtk.main
