3d10cdf12c9c5f5250b9123a9772b023cc5ce95d
Arafat <arafat@iitkgp.ac.in>
2018-07-05 15:47:23 -0700

    Fixing bug in Diff checking algorithm

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ def compute_rms(test_image, reference_image)
   diff = 0
   pixel_array_1 = image1.export_pixels
   pixel_array_2 = image2.export_pixels
-  for i in 0..(arr1.size - 1)
+  for i in 0..(pixel_array_1.size - 1)
     diff += ((pixel_array_1[i] - pixel_array_2[i]).abs / 3)**2
   end
   Math.sqrt(diff / (512 * 512))
