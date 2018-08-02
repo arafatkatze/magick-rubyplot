1307e156145fd48965e7eed329ca417a7d5125f0
Arafat <arafat@iitkgp.ac.in>
2018-08-02 20:04:29 +0530

    Adding Subplots

--- lib/rubyplot/artist/subplots.rb
+++ lib/rubyplot/artist/subplots.rb
@@ -1,15 +1,73 @@
 module Rubyplot
   class SubPlot
-    # Makes a grid of M x N dimensions
+    # Makes a grid of M x N dimensions.
+    # M -> Num of vertical images.
+    # N -> Num of horizontal images.
     def initialize(m, n)
       @m = m
       @n = n
       @grid_array = Array.new((m * n), nil)
     end
 
-    # adds a plot at a given index
+    # Adds a plot at a given index
     def addplot(index, plot)
-      @grid_array[index] = plot
+      index -= 1
+      if @grid_array[index].nil?
+        plot.write
+        @grid_array[index] = plot.base_image
+      end
+    end
+
+    # Adds a image at a given index
+    def addimage(index, image)
+      index -= 1
+      @grid_array[index] = image if @grid_array[index].nil?
+    end
+
+    def max(a, b)
+      a > b ? a : b
+    end
+
+    def write(filename)
+      @max_x_pixels = 0
+      @max_y_pixels = 0
+      for i in 0..(@m - 1)
+        x_pixel_sum = 10
+        for j in 0..(@n - 1)
+          index = i * @n + j
+          x_pixel_sum += @grid_array[index].columns unless @grid_array[index].nil?
+          x_pixel_sum += 10
+        end
+        @max_x_pixels = max(@max_x_pixels, x_pixel_sum)
+      end
+
+      for j in 0..(@n - 1)
+        y_pixel_sum = 10
+        for i in 0..(@m - 1)
+          index = i * @n + j
+          y_pixel_sum += @grid_array[index].rows unless @grid_array[index].nil?
+          y_pixel_sum += 10
+        end
+        @max_y_pixels = max(@max_y_pixels, y_pixel_sum)
+      end
+      base_image = Magick::Image.new(@max_x_pixels, @max_y_pixels) { self.background_color = 'white' }
+
+      y_pixel = 10
+      for i in 0..(@m - 1)
+        x_pixel_sum = 10
+        y_pixel_max = 0
+        for j in 0..(@n - 1)
+          index = i * @n + j
+          next if @grid_array[index].nil?
+          base_image = base_image.composite(@grid_array[index], Magick::ForgetGravity, x_pixel_sum, y_pixel, Magick::OverCompositeOp)
+          x_pixel_sum += @grid_array[index].columns
+          x_pixel_sum += 10
+          y_pixel_max = max(y_pixel_max, @grid_array[index].rows)
+        end
+        y_pixel += y_pixel_max + 10
+
+      end
+      base_image.write(filename)
     end
   end
 end
