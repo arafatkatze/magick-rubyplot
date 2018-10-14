c9efb532cd37f602be5de6d8d0527d674c86e8da
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-10-14 16:33:16 +0900

    massive refactoring underway. Assimilated classes and bringing uniformity to code structure

---
+++ lib/grruby/scatter.rb
@@ -0,0 +1,9 @@
+module Rubyplot
+  module GR
+    class Scatter
+      def data x_data=[], y_data=[], label: :default, color: :default
+        
+      end
+    end
+  end
+end
---
+++ lib/rubyplot/figure/geometry.rb
@@ -0,0 +1,9 @@
+module Rubyplot
+  class Figure
+    # TODO: extract instance variables from Figure into this class. Maybe rename.
+    class Geometry
+      def initialize 
+      end
+    end
+  end
+end
