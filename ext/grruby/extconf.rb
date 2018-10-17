a2bbd308d18c2b75056846116e24ac7c259d804c
Arafat <arafat@iitkgp.ac.in>
2018-10-17 13:08:40 +0530

    Refactoring Codebase 1

--- ext/grruby/extconf.rb
+++ ext/grruby/extconf.rb
@@ -1,7 +1,6 @@
 require 'mkmf'
 
-$CFLAGS << " -I/usr/local/gr/include "
-$LDFLAGS << " -L/usr/local/gr/lib -lGR -lm -Wl,-rpath,/usr/local/gr/lib "
-
+$CFLAGS << ' -I/usr/local/gr/include '
+$LDFLAGS << ' -L/usr/local/gr/lib -lGR -lm -Wl,-rpath,/usr/local/gr/lib '
 
 create_makefile('grruby/grruby')
